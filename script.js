47517736965617402f82ec576095ac701f92791f
Jasper Alblas <just_jasper@hotmail.com>
2020-12-06 20:00:28 +0100

    adds option to add colors

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-function setupBoard(gridSize) {
+function setupBoard() {
     // your javascript file
     const container = document.querySelector('#container');
+
+    gridSize = document.querySelector('input').value;
+
     container.style.setProperty("--columns-row", gridSize);
 
     for (let i = 0; i < gridSize * gridSize; i++) {
         const content = document.createElement('div');
         content.classList.add('box');
 
+        let color = document.querySelector('#colorPicker').value;
+
         content.addEventListener('mouseover', (event) => {
-            event.target.style.backgroundColor = "black";
+            if (color === "color") {
+                event.target.style.backgroundColor = "#"+((1<<24)*Math.random()|0).toString(16);
+            } else {
+                event.target.style.backgroundColor = "black";
+            }
+            
         })
     
         container.appendChild(content);
@@ -25,7 +35,7 @@ function setupBoard(gridSize) {
 function removeBoard() {
     const container = document.querySelector('#container');
     container.innerHTML = "";
-    setupBoard(16);
+    setupBoard();
 }
 
-setupBoard(16);
+setupBoard();
